a5101a3a94d5602784e8a32ca58177705dd59519
lucy-sees <100368317+lucy-sees@users.noreply.github.com>
2022-03-11 22:48:08 +0300

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,7 +8,7 @@
 
  /** 
 
-  * main - Determine if the last digit of a random number is 
+  * function main - Determines if the last digit of a random number is 
 
   * greater than or less than 5, or is zero.. 
 
