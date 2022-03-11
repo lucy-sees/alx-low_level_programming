721e9f22309186a2aa80b840cd3a02035c51ae41
lucy-sees <lucyjay908@gmail.com>
2022-03-11 01:56:29 -0800

    commit3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,24 @@
 #!/bin/bash
 #include <stdio.h>
 
+
+
 /**
-*main
-*
-*Return: 0
-*/
+
+ * main - Entry point
+
+ *
+
+ * Return: Always 0 (Success)
+
+ */
+
 int main(void)
+
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return(0);
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
 }
