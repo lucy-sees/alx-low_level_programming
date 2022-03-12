9fba511e0f5daf7276c094b9bb2644850e8d72cb
lucy-sees <100368317+lucy-sees@users.noreply.github.com>
2022-03-12 14:25:00 +0400

    Update 9-print_comb.c
    updated file

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,1 +1,30 @@
-#!/bin/bash
+/*
+ * File: 9-print_comb.c
+ * Auth: Brennan D Baraban
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Functions by printing all possible combinations of single-digit numbers.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
+		
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
