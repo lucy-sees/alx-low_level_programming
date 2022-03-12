9a43cd2430628c7984e81765d52e3a5a0cf73617
lucy-sees <100368317+lucy-sees@users.noreply.github.com>
2022-03-12 13:57:08 +0400

    Update 6-print_numberz.c
    updated file

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,1 +1,17 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *Return: Always 0.
+ */
+int main(void)
+{
+int num;
+for (num = '0'; num <= '9'; num++)
+{
+putchar(num);
+}
+putchar('\n');
+return (0);
+}
