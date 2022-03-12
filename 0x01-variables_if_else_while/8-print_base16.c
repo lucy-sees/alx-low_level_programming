573aa4748e27f62c3394598ef60e500e6d0b3ee1
lucy-sees <100368317+lucy-sees@users.noreply.github.com>
2022-03-12 14:05:01 +0400

    Update 8-print_base16.c
    updated file

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,1 +1,18 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - functions by printing alphabets in reverse order
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
