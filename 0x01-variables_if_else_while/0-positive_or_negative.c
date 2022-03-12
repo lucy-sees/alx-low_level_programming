53a632b4e9c0252a9f6ad7245a775e7001b8e318
lucy-sees <100368317+lucy-sees@users.noreply.github.com>
2022-03-12 14:38:05 +0400

    Update 0-positive_or_negative.c
    update file changes

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,1 +1,31 @@
-#!/bin/bash
+/*
+ * File: 0-positive_or_negative.c
+ * Author: Lucy W. Mwangi 
+ */
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Functions by printing random number and states whether
+ *        it is positive, negative, or zero.
+ *
+ * Return: Always 0.
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is zero\n", n);
+
+	return (0);
+}
