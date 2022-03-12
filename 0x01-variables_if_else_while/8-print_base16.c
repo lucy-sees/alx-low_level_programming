7e79ffe04caa640ee37aa559b73e567bf79ef43b
lucy-sees <100368317+lucy-sees@users.noreply.github.com>
2022-03-12 14:18:53 +0400

    Update 8-print_base16.c
    update file

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 
 /**
- * main - functions by printing alphabets in reverse order
+ * main - Functions by printing all the numbers of base 16 in lowercase
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	char letter;
+int num;
+char letter;
 
-	for (letter = 'z'; letter >= 'a'; letter--)
-		putchar(letter);
+for (num = 0; num < 10; num++)
+putchar((num % 10) + '0');
+for (letter = 'a'; letter <= 'f'; letter++)
+putchar(letter);
 
-	putchar ("\n");
-
-	return (0);
+putchar('\n');
+return (0);
 }
