f26f05ac7d59b722496f224a42a2a53468a787f5
lucy-sees <100368317+lucy-sees@users.noreply.github.com>
2022-03-12 12:18:41 +0400

    Update 4-print_alphabt.c
    updated this file

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,18 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - The main block
+ *
+ * Return: Always 0 is success
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+if (ch != 'e' && ch != 'q')
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
