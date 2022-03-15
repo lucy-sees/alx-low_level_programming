bd3f8b63adb5e5e343ffe208682df34ed3459aa6
lucy-sees <100368317+lucy-sees@users.noreply.github.com>
2022-03-16 00:51:35 +0300

    Update 0-putchar.c
    Commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,11 @@
-#include "_putchar.h"
+#include <unistd.h>
 /**
- * main - Prints _putchar
+ * main - Entry point of code
  *
  * Return: Always 0 (Success)
  */
-int main(void)
-{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
-}
+
+ ​int​ ​_putchar​(​char​ c) 
+ ​{ 
+ ​        ​return​ (​write​(​1​, &c, ​1​)); 
+ ​}
