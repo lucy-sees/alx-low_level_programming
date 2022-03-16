cb5302a27448b27b8df2350e265b73f3470c1307
lucy-sees <100368317+lucy-sees@users.noreply.github.com>
2022-03-16 03:36:15 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,5 @@
- ​#​include​ ​<​unistd.h​> 
- ​#​include​ ​"​main.h​" 
+#​include​ ​<​unistd.h​> 
+#​include​ ​"​main.h​" 
   
  ​/*​* 
  ​ * main - Check description 
@@ -7,14 +7,15 @@
  ​ * Return: 0. 
  ​ ​*/ 
   
- ​int​ ​main​(​void​) 
- ​{ 
- ​        ​char​ word[​9​] = ​"​_putchar​\n​"​; 
- ​        ​int​ i; 
+int​ ​main​(​void​) 
+{
+	char​ word[​9​] = ​"​_putchar​\n​"​; 
+ ​ int​ i; 
   
- ​        ​for​(i=​0​; i<​9​; i++) 
- ​        { 
- ​                ​_putchar​(word[i]); 
- ​        } 
- ​        ​return​(​0​); 
- ​}
+ ​ for​(i=​0​; i<​9​; i++)
+   }
+ 		_putchar​(word[i]);
+ 	}
+ 	
+ 	return​(​0​);
+}
