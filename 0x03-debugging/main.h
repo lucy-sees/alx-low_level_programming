e5a3a1e0e09ff7c810f72c004d320e17a99e67b9
lucy-sees <100368317+lucy-sees@users.noreply.github.com>
2022-03-18 00:14:15 +0300

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,8 +1,11 @@
-#​ifndef​ MAIN.H
-#​define​ ​MAIN​.H
-#​include​ ​<​stdio.h​>
-​void​ ​positive_or_negative​(​int​ i);
-​int​ ​largest_number​(​int​ a, ​int​ b, ​int​ c);
-​void​ ​print_remaining_days​(​int​ month, ​int​ day, ​int​ year);
-​int​ ​convert_day​(​int​ month, ​int​ day);
-​#​endif​ ​/*​ MAIN.H ​*/
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+int _putchar(char c);
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+
+#endif
