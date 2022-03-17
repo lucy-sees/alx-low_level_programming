aaeafa23095e2241db41d4cc5547fca2bfa5675f
lucy-sees <100368317+lucy-sees@users.noreply.github.com>
2022-03-17 10:48:06 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,25 @@
-/**
- * main - check the code.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    print_alphabet_x10();
-    return (0);
+#include​ ​"main.h​"
+​/*​*
+ ​ * print_alphabet_x10 - Print alphabet in lowercase 10 times
+ ​ * Return: 0
+ ​ ​*/
+​void​ ​print_alphabet_x10​(​void​)
+​ {
+    ​        ​int​ i = ​0​;
+    ​        ​char​ c;
+
+    ​        ​while​ (i < ​10​)
+        ​        {
+        ​                c = ​'​a​'​;
+        ​                ​while​ (c <= ​'​z​'​)
+            ​                {
+            ​                        ​_putchar​(c);
+            ​                        c++;
+            ​                
+        }
+        ​                ​_putchar​(​'​\n​'​);
+        ​                i++;
+        ​        
+    }
+    ​
+}
