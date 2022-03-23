cfe4d042bb078b6d03c8c893da9e5c2cb1d3fa43
lucy-sees <100368317+lucy-sees@users.noreply.github.com>
2022-03-23 11:48:43 +0300

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,14 @@
 #include "main.h"
+
 /**
- * main - main function
- * Return: 0
+ *main - main function
+ *Return: 0
  */
 
 int main(void)
 {
 	int i = 0;
-	
+
 	positive_or_negative(i);
 	return (0);
 }
