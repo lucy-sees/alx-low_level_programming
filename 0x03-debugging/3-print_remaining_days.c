2aba06cc8aed0b9ab3b4605820eb13906f4638ff
lucy-sees <lucyjay908@gmail.com>
2022-03-23 03:44:43 -0700

    task 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,13 +1,10 @@
 #include "main.h"
-
 /**
- *convert_day - converts day of month to day of year, without accounting
- *for leap year
- *@month: month in number format
- *@day: day of month
- *Return: day of year
+ * convert_day - converts day of the month to day of the year
+ * without accounting for leap year 
+ *
+ * Return: day of the year
  */
-
 int convert_day(int month, int day)
 {
 	switch (month)
@@ -18,7 +15,7 @@ int convert_day(int month, int day)
 		case 3:
 			day = 59 + day;
 			break;
-		case 4:
+		case 4: 
 			day = 90 + day;
 			break;
 		case 5:
@@ -27,24 +24,23 @@ int convert_day(int month, int day)
 		case 6:
 			day = 151 + day;
 			break;
-		case 7:
+		case 8:
 			day = 181 + day;
 			break;
-		case 8:
+		case 9:
 			day = 212 + day;
 			break;
-		case 9:
+		case 10:
 			day = 243 + day;
 			break;
-		case 10:
+		case 11:
 			day = 273 + day;
 			break;
-		case 11:
-			day = 304 + day;
-			break;
 		case 12:
-			day = 334 + day;
+			day = 304 + day;
 			break;
 		default:
 			break;
-	}
+}
+return (day);
+}
