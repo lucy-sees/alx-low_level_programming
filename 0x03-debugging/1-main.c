1d8b82b33ebde893a202708994d0359c452aace5
lucy-sees <100368317+lucy-sees@users.noreply.github.com>
2022-03-23 11:54:36 +0300

    Update 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -3,22 +3,24 @@
 /**
  *main - infinite loop
  *
- * Return: 0
+ *Return: 0
  */
-int main(void) {
-  int i;
-  printf("Infinite loop incoming :(\n");
+int main(void)
+{
+	int i;
 
-  i = 0;
+	printf("Infinite loop incoming :(\n");
+	i = 0;
 
-  /*
-   * while (i < 10)
-   * {
-   * Infinite loop - No increase of variable
-   * putchar(i)
-  }
-  */
+	/*
+	 *while (i < 10)
+	 * {
+	 *Infinite loop - No increase of
+	 variable
+	 *putchar(i)
+	}
 
-  printf("Infinite loop avoided! \\o/\n");
-  return (0);
+	*/
+	printf("Infinite loop avoided! \\o/\n");
+	return (0);
 }
