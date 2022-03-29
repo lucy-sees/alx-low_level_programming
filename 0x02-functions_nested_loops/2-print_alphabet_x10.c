0c75d9b261b28457640fc8d47974c823d6f9307e
lucy-sees <100368317+lucy-sees@users.noreply.github.com>
2022-03-29 20:08:29 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,7 +6,7 @@
  *Description: prints 10 times
  * Return: 0 (Success)
  */
-void print_alphabet_x10.c(void)
+void print_alphabet_x10(void)
 {
 	int alphabet_count = 0;
 	while (alphabet_count < 10)
