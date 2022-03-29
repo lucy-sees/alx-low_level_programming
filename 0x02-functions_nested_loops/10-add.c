375aa8ee4385c72991d17696012f2f06f61c4f0f
lucy-sees <100368317+lucy-sees@users.noreply.github.com>
2022-03-29 21:23:03 +0300

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,13 +2,12 @@
 
 /**
  * add - add two numbers from input
- * 
  *
  * Description: adds two integers and returns the result.
  * Return: Always (0).
  */
 
-int add(int a, int b)
+int add(int a, int b)/*a, b are parameters*/
 {
 	return (a + b);
 }
