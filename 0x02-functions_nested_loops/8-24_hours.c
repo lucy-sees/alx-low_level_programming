9e165d46829f30cb57c6875c775fbaf397bdf298
lucy-sees <100368317+lucy-sees@users.noreply.github.com>
2022-03-29 20:54:59 +0300

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,9 +1,26 @@
+#include "main.h"
+
 /**
- * main - check the code
+ * prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
  *
- * Return: Always 0.
+ * Description: prints a list of time
+ * Return: Always 0(success)
  */
-int main(void)
+
+void jack_bauer(void)
 {
-    jack_bauer();
-    return (0);
+	int i, j;
+
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			_putchar(i / 10 + 48);
+			_putchar(i % 10 + 48);
+			_putchar(':');
+			_putchar(j / 10 + 48);
+			_putchar(j % 10 + 48);
+			_putchar('\n');
+		}
+	}
+}
