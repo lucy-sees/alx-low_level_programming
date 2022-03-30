4ced6301894fadf19a22a1dbdeae6bcfeda5bed1
lucy-sees <lucyjay908@gmail.com>
2022-03-30 06:32:34 -0700

    Task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+/* Author: Lucy W. Mwangi*/
+#include "main.h"
+/**
+ * _print_rev_recursion - a function that prints a string in reverse.
+ *
+ * Return: Always 0
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
