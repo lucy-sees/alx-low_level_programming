42cfe8b77c0829dbbe3d49e277ca10149e164bae
lucy-sees <100368317+lucy-sees@users.noreply.github.com>
2022-03-30 13:23:30 +0300

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,21 +1,23 @@
-include "main.h"
+/*Author: Lucy W. Mwangi .... Task 5*/
+#include "main.h"
 
 /**
- * _strstr - finds the first occurrence of the substring needle in the string haystack
+ * _strstr - first occurrence of the substring needle in the string haystack
  * @haystack: main str to be examined
  * @needle: searched in haystack
  * Return: return 0
  **/
 
-char *_strstr(char *haystack, char *needle)
+char  *_strstr(char *haystack, char *needle)
 {
 	char *str1, *str2; /*Declaring variables*/
 
 	while (*haystack != '\0')
 	{
-		str1 = haystack; 
+		str1 = haystack; /*values*/
 		str2 = needle;
-		
+
+		/*Star WHILE*/
 		while (*haystack != '\0' && *str2 != '\0' && *haystack == *str2)
 		{
 			haystack++;
