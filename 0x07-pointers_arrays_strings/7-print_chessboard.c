0b5760b274655fe02a54d6f96940f270dc472223
lucy-sees <lucyjay908@gmail.com>
2022-03-30 03:51:52 -0700

    Task 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,28 @@
+/*Author: Lucy W. Mwangi ..... Task 6*/
+#include "main.h"
+/*we call our libraries from our initially created header file, main.h*/
+
+/**
+ * print_chessboard - prints a chessboard
+ * @a: 2D array of characters
+ * Return: 0 (Success)
+ **/
+void print_chessboard(char (*a)[8]) /*function prototype called from main.h*/
+{
+	/*declaring variables*/
+	int i, j;
+	
+	i = 0;
+	while (i < 8) /*number repetitions*/
+	{
+		j = 0;
+		while (j < 8)
+		{
+			_putchar (a[i] [j]);
+			
+			j++;
+		}
+		_putchar ('\n'); /*print new line*/
+		i++; /*add +1/
+	}
+}
