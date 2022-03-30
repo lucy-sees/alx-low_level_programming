635f784c23ca45717d513a390fba377d5a14acbd
lucy-sees <lucyjay908@gmail.com>
2022-03-30 07:19:36 -0700

    Task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,27 @@
+/* Author: Lucy W. Mwangi ..... Task 5 */
+#include "main.h"
+/**
+ * _evaluate - returns the value of x raised to the power of y.
+ * @i: interger
+ * @n: interger
+ * Return: sqrt
+ */
+
+int _evaluate(int i, int n)
+
+{
+	if (n == 0 || n == 1)
+	{
+		return (n);
+	}
+	else if (i * i < n)
+	{
+		return (_evaluate(i + 1, n));
+	}
+	else if (i * i == n)
+	{
+		return (i);
+	}
+	rreturn (-1);
+	return (-1);
+}
