fe70302754bbba75210a4e0cc65ae520488f2fca
lucy-sees <lucyjay908@gmail.com>
2022-03-30 09:33:42 -0700

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+
+/* Author: Lucy W. Mwangi ..... Task 2 */
+#include "main.h"
+/**
+ * _strlen - calculate the length of a string.
+ *
+ * @s: the string to calculate it's length.
+ * Return: length of a string.
+ */
+
+int _strlen(char *s)
+{
+	int i;
+	int length = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		length++;
+	}
+	return (length);
+}
