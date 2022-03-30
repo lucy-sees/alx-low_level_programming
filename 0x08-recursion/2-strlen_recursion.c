019e6922422e3a9119eb03753c8a4f3ff4fd9ee2
lucy-sees <100368317+lucy-sees@users.noreply.github.com>
2022-03-30 16:48:43 +0300

    Update 2-strlen_recursion.c
    correction

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,16 +1,23 @@
 /** Author: Lucy W. Mwangi ..... Task 2 */
+
 #include "main.h"
-/** 
- *
- *
- *
- *
+
+/**
+ *_strlen_recursion - length of a string
+ *@s: pointer block of memory to fill
+ *Return: strlen_recursion
  */
+
 int _strlen_recursion(char *s)
 {
+
+	/*Base condition*/
 	if (*s == '\0')
-	return (0);
-	
+	{
+		return (0);
+	}
 	else
-	return (1 + _strlen_recursion(s + 1));
+	{
+		return (1 + _strlen_recursion(s + 1)); /*Sum 1*/
+	}
 }
