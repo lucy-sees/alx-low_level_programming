6f8697b2432a0585bef5036821b958ce56052c67
lucy-sees <100368317+lucy-sees@users.noreply.github.com>
2022-03-30 17:22:29 +0300

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,5 @@
 /* Author: Lucy W. Mwangi ..... Task 5 */
+
 #include "main.h"
 /**
  * _evaluate - returns the value of x raised to the power of y.
@@ -22,6 +23,26 @@ int _evaluate(int i, int n)
 	{
 		return (i);
 	}
-	rreturn (-1);
 	return (-1);
+	return (-1);
+}
+
+/**
+ *_sqrt_recursion - evaluate sqrt
+ *@n: interger
+ *Return: Sgrt_recursion
+*/
+
+int _sqrt_recursion(int n)
+{
+	int i = 0;
+	if (i < 0) 
+	{
+		return (-1);
+	}
+	else
+	{
+		return (_evaluate(i, n));
+	}
+
 }
