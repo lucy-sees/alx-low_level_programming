8939d5972bd7db753ada7282e00813ac5d73a722
lucy-sees <lucyjay908@gmail.com>
2022-03-30 06:58:53 -0700

    Task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+/* Author: Lucy W. Mwangi .... Task 4 */
+#include "main.h"
+
+/**
+ *_pow_recursion - a function that returns the value of x raised to the power of y.
+ *@x: integer
+ *@y: integer
+ *Return: pow recursion
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y != 0)
+	return (x * _pow_recursion(x, y - 1));
+	
+	else
+	return (1);
+}
