672aa9d1ab22d493560fb45656dfff2104bbfd8e
lucy-sees <100368317+lucy-sees@users.noreply.github.com>
2022-03-30 18:21:05 +0300

    Update 101-wildcmp.c
    commit changes

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,28 +1,29 @@
 /* Author: Lucy W. Mwangi ...... Task 8 */
-
 #include "main.h"
+
 /**
- * wildcmp - returns 1 if a string is a palindrome and 0 if not.
+ * wildcmp - Compare strings
  * @s1: pointer to string params
  * @s2: pointer to string params
  * Return: 0
  */
 
-int wildcmp(char *s1, char *s2)
+int wildcmp(char * s1, char * s2)
 {
-  if (*s1 == '\0')
+  if ( * s1 == '\0')
   {
-    if (*s2 != '\0' && *s2 == '*')
+    if ( * s2 != '\0' && * s2 == '*')
     {
       return (wildcmp(s1, s2 + 1));
     }
-    return (*s2 == '\0');
+    return ( * s2 == '\0');
   }
-  if (*s2 == '*')
+
+  if ( * s2 == '*')
   {
     return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
-  }
-  else if (*s1 == *s2)
+  } 
+  else if ( * s1 == * s2)
   {
     return (wildcmp(s1 + 1, s2 + 1));
   }
