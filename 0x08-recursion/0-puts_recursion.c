643c317c3ee851f51ba9273b56f208a2b71df7e4
lucy-sees <100368317+lucy-sees@users.noreply.github.com>
2022-03-30 16:18:44 +0300

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,18 +1,21 @@
 /* Author: Lucy W. Mwangi ..... Task 0 */
 #include "main.h"
+
 /**
- * _puts_recursion - prints a string followed by a new line
- * @s: string to print
- *
- * Return: 0 (success)
+ * _puts_recursion - prints a string, followed by a new line.
+ * @s: pointer to char
+ * Return: Always 0.
  */
-void _puts_recursion(char *s)
+
+void _puts_recursion(char *s) /* function prototype*/
 {
-	if (*s == '\0')
+	if (*s != '\0')
 	{
-		_putchar('\n');
-		return;
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
-	_putchar(*s);
-	_puts_recursion(s + 1);
+	else
+	{
+	_putchar('\n');
+	}
 }
