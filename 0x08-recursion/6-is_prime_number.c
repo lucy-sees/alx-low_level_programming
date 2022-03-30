538a1a12aa649b98e6ad870f8836d3f3f2d429a8
lucy-sees <100368317+lucy-sees@users.noreply.github.com>
2022-03-30 17:56:30 +0300

    Task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,24 +9,26 @@
  *@iterator: number to iterate
  *Return: return 1 or 0
  */
-
 int evaluate_num(int num, int iterator)
 {
-  if (iterator == num - 1)
-  {
-    return (1);
-  }
 
-  else if (num % iterator == 0)
-  {
-    return (0);
-  }
+if (iterator == num - 1)
+{
+return (1);
+}
 
-  if (num % iterator != 0)
-  {
-  return (evaluate_num(num, iterator + 1));
-  }
-  return (0);
+else if (num % iterator == 0)
+{
+return (0);
+}
+
+if (num % iterator != 0)
+{
+return (evaluate_num(num, iterator + 1));
+}
+
+return (0);
+
 }
 
 /**
@@ -38,19 +40,20 @@ int evaluate_num(int num, int iterator)
 int is_prime_number(int num)
 {
 
-  int iterator;
+int iterator;
 
-  iterator = 2;
+iterator = 2;
 
 /* only greater than 2*/
-  if (num < 2)
-  {
-  return (0);
-  }
+if (num < 2)
+{
+return (0);
+}
 
-  if (num == 2)
-  {
-  return (1);
-  }
-  return (evaluate_num(num, iterator));
+if (num == 2)
+{
+return (1);
+}
+
+return (evaluate_num(num, iterator));
 }
