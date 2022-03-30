7dbd228cc9c421ccf27d700e79f79bf5c785644a
lucy-sees <100368317+lucy-sees@users.noreply.github.com>
2022-03-30 13:57:12 +0300

    Update 7-print_chessboard.c
    fixed some errors

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -11,18 +11,19 @@ void print_chessboard(char (*a)[8]) /*function prototype called from main.h*/
 {
 	/*declaring variables*/
 	int i, j;
-	
+
 	i = 0;
 	while (i < 8) /*number repetitions*/
 	{
 		j = 0;
 		while (j < 8)
 		{
-			_putchar (a[i] [j]);
-			
+			_putchar (a[i][j]);
+		
 			j++;
 		}
 		_putchar ('\n'); /*print new line*/
-		i++; /*add +1/
+		
+		i++; /*add +1*/
 	}
 }
