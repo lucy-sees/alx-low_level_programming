bf3ff42e298d7700aacbd30e0fdd68336c112ce0
lucy-sees <100368317+lucy-sees@users.noreply.github.com>
2022-03-30 17:52:00 +0300

    Task 6
    correction

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,56 @@
+/* Author: Lucy W. Mwangi ..... Task 6 */
+
+#include "main.h"
+
+/**
+ *evaluate_num - returns 1 if the input integer is a prime number,
+ otherwise return 0.
+ *@num: num
+ *@iterator: number to iterate
+ *Return: return 1 or 0
+ */
+
+int evaluate_num(int num, int iterator)
+{
+  if (iterator == num - 1)
+  {
+    return (1);
+  }
+
+  else if (num % iterator == 0)
+  {
+    return (0);
+  }
+
+  if (num % iterator != 0)
+  {
+  return (evaluate_num(num, iterator + 1));
+  }
+  return (0);
+}
+
+/**
+ *is_prime_number - evaluate prime or not
+ *@num: number
+ *Return: return 1 prime - return 0 otherwise
+ */
+
+int is_prime_number(int num)
+{
+
+  int iterator;
+
+  iterator = 2;
+
+/* only greater than 2*/
+  if (num < 2)
+  {
+  return (0);
+  }
+
+  if (num == 2)
+  {
+  return (1);
+  }
+  return (evaluate_num(num, iterator));
+}
