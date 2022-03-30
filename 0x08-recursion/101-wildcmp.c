8cdc0564bfb65743a1b7162a60325f910d603604
lucy-sees <100368317+lucy-sees@users.noreply.github.com>
2022-03-30 18:44:04 +0300

    Update 101-wildcmp.c
    corrections

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,30 +1,32 @@
-/* Author: Lucy W. Mwangi ..... Task 8 */
-#include "main.c"
+/* Author: Lucy W. Mwangi ...... Task 8 */
+
+#include "main.h"
+
 /**
- * wildcmp - Compare strings
- * @s1: pointer to string 1
- * @s2: pointer to string 2
+ * wildcmp - returns 1 if a string is a palindrome and 0 if not.
+ * @s1: pointer to string params
+ * @s2: pointer to string params
  * Return: 0
  */
 
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == '\0')
-	{
-		if (*s2 != '\0' && *s2 == '*')
-		{
-			return (wildcmp(s1, s2 + 1));
-		}
-		return (*s2 == '\0');
-	}
+if (*s1 == '\0')
+{
+if (*s2 != '\0' && *s2 == '*')
+{
+return (wildcmp(s1, s2 + 1));
+}
+return (*s2 == '\0');
+}
 
-	if (*s2 == '*')
-	{
-		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
-	}
-	else if (*s1 == *s2)
-	{
-		return (wildcmp(s1 + 1, s2 + 1));
-	}
-	return (0);
+if (*s2 == '*')
+{
+return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+}
+else if (*s1 == *s2)
+{
+return (wildcmp(s1 + 1, s2 + 1));
+}
+return (0);
 }
